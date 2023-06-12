d709716125e5008a58bffe29dc9c16cb038131d9
Steven Tieu <103811649+SteveT10@users.noreply.github.com>
2023-06-11 19:26:47 -0700

    Changed 3d visuals to match mock up

--- scripts/background.js
+++ scripts/background.js
@@ -1,13 +1,7 @@
 import * as THREE from 'three';
 //Importmap recognizes three/addons
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-
-/*
-      "imports": {
-        "three": "https://unpkg.com/three@v0.152.2/build/three.module.js",
-        "three/addons/": "https://unpkg.com/three@v0.152.2/examples/jsm/"
-      }
-*/
+import WebGL from 'three/addons/capabilities/WebGL.js';
 
 //Shapes.
 let cube, cubeOfCubes;
@@ -24,49 +18,57 @@ const camControls =  new OrbitControls(camera, renderer.domElement);
 
 const scene = new THREE.Scene();
 
+/**********HELPER VISUALS (DELETE BEFORE FINAL RELEASE)**********/
+//x, y, z axes, points in positive direction.
+const axes = new THREE.AxesHelper(5)
+axes.setColors('red', 'blue', 'green'); //x = r, y = b, z = g.
+scene.add(axes);
+
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 
+
 /***********CAMERA AND ITS CONTROLS***********/
 //By default, the camera will be looking down -Z with, positioned at (0, 0, 0)
-camera.position.set(2, 1, 15); //x, y, z
-camControls.update();
+camera.position.set(20, 20, 20); //x, y, z
+camControls.update(); //update everytime position changes.
 
 camControls.listenToKeyEvents( window );
 
-//Damping slows down camera speed?
+//Damping slows down camera speed
 camControls.enableDamping = true;
-camControls.dampingFactor = 0.05;
+camControls.dampingFactor = 0.025;
 
 camControls.screenSpacePanning = false;
 
 //Min and Max distance we can zoom on camera
 camControls.minDistance = 0;
 camControls.maxDistance = 50;
-camControls.maxPolarAngle = Math.PI / 2;
+camControls.maxPolarAngle = Math.PI / 2; //90 degrees
 
-camControls.enablePan = true;
-camControls.enableRotate = true;
 camControls.enableZoom = true;
-
+onWindowResize(); //Calc aspect for first time.
 //camControls.update() must be called after any manual changes to the camera's transform
 
-scene.background =  new THREE.Color(0xcc33ff);
+scene.background =  new THREE.Color(0xCFD8DC);
 
 /***********LIGHTING***********/
 {
     const color = 0xFFFFFF;
     const intensity = 1;
     const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(-1, 2, 4);
+    //light.position.set(-1, 2, 4);
+    light.position.set(-1, 5, 3);
+    //By default the light will look at (0, 0, 0), change with light.target()
     scene.add(light);
 }
 
 
 /***********SHAPES***********/
-cube =  makeCube(1, 1, 1, 0x44aa88, 2, 2, 2);
-cubeOfCubes = makeCubeOf(cube, 2, 2, 2, 3, 3, 3);
+cube =  makeCube(1, 1, 1, 'white', 2, 2, 2);
+cubeOfCubes = makeCubeOf(cube, 1.5, 1.5, 1.5, 10, 10, 10);
 //renderer.render(scene, camera); //Renders cube once.
 
 /***********EVENTS***********/
@@ -74,10 +76,12 @@ window.addEventListener( 'resize', onWindowResize);
 
 function animate(time) {
     time *= 0.001; //convert to seconds.
+
+    //Rotate cubes.
     //myCube.rotation.x = time;
     //myCube.rotation.y = time;
     cubeOfCubes.forEach((cube, ndx) => {
-        const speed = 1 + ndx * .1;
+        const speed = 0.5;
         const rot = time * speed;
         cube.rotation.x = rot;
         cube.rotation.y = rot;
@@ -89,8 +93,14 @@ function animate(time) {
     renderer.render(scene, camera);
     //requestAnimationFrame passes time since the page loaadeed to our function.
 }
-requestAnimationFrame(animate); //Starts loop, continously calls requestAnimationFrame() on animate.
-// }
+
+if ( WebGL.isWebGLAvailable() ) {
+    //Starts loop, continously calls requestAnimationFrame() on animate.
+    requestAnimationFrame(animate);
+} else {
+	const warning = WebGL.getWebGLErrorMessage();
+	document.getElementById( 'container' ).appendChild( warning );
+}
 
 
 function makeCube(boxW, boxH, boxD, color, xPos, yPos, zPos) {
