a286ff1809248fac46391bd3cc7b126392688dda
Steven Tieu <103811649+SteveT10@users.noreply.github.com>
2023-06-17 18:22:47 -0700

    reformat background.js

--- scripts/background.js
+++ scripts/background.js
@@ -8,16 +8,16 @@ import { BloomEffect, PixelationEffect, EffectComposer, EffectPass, RenderPass }
 //Shapes.
 let cube, cubeOfCubes;
 
-const backCanvas =  document.querySelector('#c');
+const backCanvas = document.querySelector('#c');
 
 // alpha true defaults to transparent bg
-const renderer =  new THREE.WebGL1Renderer({alpha: true, antialias: true, backCanvas});
+const renderer = new THREE.WebGL1Renderer({ alpha: true, antialias: true, backCanvas });
 
 //(fov, aspect, minRender, maxRender);
 const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 100);
 
 //Controls for Camera, add-on for three.js
-const camControls =  new OrbitControls(camera, renderer.domElement);
+const camControls = new OrbitControls(camera, renderer.domElement);
 
 const scene = new THREE.Scene();
 
@@ -35,9 +35,9 @@ const cubeGaps = [1.5, 1.5, 1.5];
 // })
 
 //Instancing test.
-const testShape =  new THREE.BoxGeometry();
-const testMaterial = new THREE.MeshPhongMaterial({color: 0xFFFFFF});
-const testObj =  new THREE.InstancedMesh(testShape, testMaterial, 10000);
+const testShape = new THREE.BoxGeometry();
+const testMaterial = new THREE.MeshPhongMaterial({ color: 0xFFFFFF });
+const testObj = new THREE.InstancedMesh(testShape, testMaterial, 10000);
 
 
 scene.add(testObj);
@@ -59,13 +59,13 @@ const currShape = new THREE.Object3D();
 //     }
 // }
 
-for(let i = 0; i < 10000; i++) {
-    currShape.position.x = Math.random() * 100 - 20; //80 to -20
-    currShape.position.y = Math.random() * 100 - 20;
-    currShape.position.z = Math.random() * 100 - 20;
+for (let i = 0; i < 10000; i++) {
+  currShape.position.x = Math.random() * 100 - 20; //80 to -20
+  currShape.position.y = Math.random() * 100 - 20;
+  currShape.position.z = Math.random() * 100 - 20;
 
-    currShape.updateMatrix();
-    testObj.setMatrixAt(i, currShape.matrix);
+  currShape.updateMatrix();
+  testObj.setMatrixAt(i, currShape.matrix);
 }
 
 
@@ -87,7 +87,7 @@ document.body.appendChild(renderer.domElement);
 camera.position.set(numOfCubes[0] * 3, numOfCubes[1] * 3, numOfCubes[2] * 3); //x, y, z
 camControls.update(); //update everytime position changes.
 
-camControls.listenToKeyEvents( window );
+camControls.listenToKeyEvents(window);
 
 //Damping slows down camera speed
 camControls.enableDamping = true;
@@ -108,74 +108,74 @@ onWindowResize(); //Calc aspect for first time.
 
 /***********LIGHTING***********/
 {
-    const color = 0xFFFFFF;
-    const intensity = 1;
-    const light = new THREE.DirectionalLight(color, intensity);
-    //light.position.set(-1, 2, 4);
-    light.position.set(-1, 5, 3);
-    //By default the light will look at (0, 0, 0), change with light.target()
-    scene.add(light);
+  const color = 0xFFFFFF;
+  const intensity = 1;
+  const light = new THREE.DirectionalLight(color, intensity);
+  //light.position.set(-1, 2, 4);
+  light.position.set(-1, 5, 3);
+  //By default the light will look at (0, 0, 0), change with light.target()
+  scene.add(light);
 
 
-    //HELPER VISUAL 
-    const lightHelper = new THREE.DirectionalLightHelper( light, 5, 0xC52AB1);
-    // lightHelper.color = 0xC52AB1;
-    scene.add( lightHelper );
+  //HELPER VISUAL 
+  const lightHelper = new THREE.DirectionalLightHelper(light, 5, 0xC52AB1);
+  // lightHelper.color = 0xC52AB1;
+  scene.add(lightHelper);
 }
 
 
 /***********SHAPES***********/
 //cube =  makeCube(1, 1, 1, 'white', 2, 2, 2); //Pos does not matter here, since it never in scene.
 //cubeOfCubes = makeCubeOf(cube, cubeGaps[0], cubeGaps[1], cubeGaps[2],
-                         //numOfCubes[0], numOfCubes[1], numOfCubes[2]);
+//numOfCubes[0], numOfCubes[1], numOfCubes[2]);
 //renderer.render(scene, camera); //Renders cube once.
 
 /***********EVENTS***********/
-window.addEventListener( 'resize', onWindowResize);
+window.addEventListener('resize', onWindowResize);
 
 function animate(time) {
-    time *= 0.001; //convert to seconds.
+  time *= 0.001; //convert to seconds.
 
-    //Rotate cubes.
-    //myCube.rotation.x = time;
-    //myCube.rotation.y = time;
-    //cubeOfCubes.forEach((cube, ndx) => {
-        const speed = 0.5;
-        const rot = time * speed;
-        //cube.rotation.x = rot;
-        //cube.rotation.y = rot;
-    //})
-    requestAnimationFrame(animate);//Request to brower to animate something
-    camControls.update(); //Requires if(enableDamping || autoRotate)
-    
-    composer.render();
-    //renderer.render(scene, camera);
-    //requestAnimationFrame passes time since the page loaadeed to our function.
+  //Rotate cubes.
+  //myCube.rotation.x = time;
+  //myCube.rotation.y = time;
+  //cubeOfCubes.forEach((cube, ndx) => {
+  const speed = 0.5;
+  const rot = time * speed;
+  //cube.rotation.x = rot;
+  //cube.rotation.y = rot;
+  //})
+  requestAnimationFrame(animate);//Request to brower to animate something
+  camControls.update(); //Requires if(enableDamping || autoRotate)
+
+  composer.render();
+  //renderer.render(scene, camera);
+  //requestAnimationFrame passes time since the page loaadeed to our function.
 }
 
-if ( WebGL.isWebGLAvailable() ) {
-    //Starts loop, continously calls requestAnimationFrame() on animate.
-    requestAnimationFrame(animate);
+if (WebGL.isWebGLAvailable()) {
+  //Starts loop, continously calls requestAnimationFrame() on animate.
+  requestAnimationFrame(animate);
 } else {
-	const warning = WebGL.getWebGLErrorMessage();
-	document.getElementById( 'container' ).appendChild( warning );
+  const warning = WebGL.getWebGLErrorMessage();
+  document.getElementById('container').appendChild(warning);
 }
 
 
 function makeCube(boxW, boxH, boxD, color, xPos, yPos, zPos) {
-    //Shape of object
-    //(boxWidth, boxHeight, BoxDepth)
-    const cubeVertices = new THREE.BoxGeometry(boxW, boxH, boxD);
-    const material =  new THREE.MeshPhongMaterial({color});
+  //Shape of object
+  //(boxWidth, boxHeight, BoxDepth)
+  const cubeVertices = new THREE.BoxGeometry(boxW, boxH, boxD);
+  const material = new THREE.MeshPhongMaterial({ color });
 
-    //Combines Geometry, Material, and position.
-    const cube =  new THREE.Mesh(cubeVertices, material);
-    //scene.add(cube);
-    cube.position.x = xPos;
-    cube.position.y = yPos;
-    cube.position.z = zPos;
+  //Combines Geometry, Material, and position.
+  const cube = new THREE.Mesh(cubeVertices, material);
+  //scene.add(cube);
+  cube.position.x = xPos;
+  cube.position.y = yPos;
+  cube.position.z = zPos;
 
-    return cube;
+  return cube;
 }
 
 /**
@@ -189,45 +189,45 @@ function makeCube(boxW, boxH, boxD, color, xPos, yPos, zPos) {
  * @returns array contains shapes in this cube.
  */
 function makeCubeOf(shape, xGap, yGap, zGap, width, height, depth) {
-    let shapes = [];
-    let totalHeight = height * yGap;
-    let totalWidth = width * xGap;
-    let totalDepth = depth * zGap;
-    let currShape;
+  let shapes = [];
+  let totalHeight = height * yGap;
+  let totalWidth = width * xGap;
+  let totalDepth = depth * zGap;
+  let currShape;
 
-    //TODO need something more efficient than 3 for loops.
-    for(let x = 0; x <= totalWidth; x = xGap + x) {
-        for(let y = 0; y <= totalHeight; y = yGap + y) {
-            for(let z = 0; z <= totalDepth; z = zGap + z) {
-                //shapes.push(makeCube(1, 1, 1, 0x44aa88, x, y, z));
-                currShape = shape.clone();
-                currShape.position.x = x;
-                currShape.position.y = y;
-                currShape.position.z = z;
-                scene.add(currShape);
-                shapes.push(currShape);
-            }
-        }
+  //TODO need something more efficient than 3 for loops.
+  for (let x = 0; x <= totalWidth; x = xGap + x) {
+    for (let y = 0; y <= totalHeight; y = yGap + y) {
+      for (let z = 0; z <= totalDepth; z = zGap + z) {
+        //shapes.push(makeCube(1, 1, 1, 0x44aa88, x, y, z));
+        currShape = shape.clone();
+        currShape.position.x = x;
+        currShape.position.y = y;
+        currShape.position.z = z;
+        scene.add(currShape);
+        shapes.push(currShape);
+      }
     }
+  }
 
-    return shapes;
+  return shapes;
 }
 
 /**
  * Resizes window when browser resizes.
  */
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix(); //
-    renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix(); //
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 export function rotateCubes() {
-    for(let i = 0; i < 10000; i++) {
-        currShape.rotation.x += 0.5;
-    
-        currShape.updateMatrix();
-        testObj.setMatrixAt(i, currShape.matrix);
-    }
+  for (let i = 0; i < 10000; i++) {
+    currShape.rotation.x += 0.5;
+
+    currShape.updateMatrix();
+    testObj.setMatrixAt(i, currShape.matrix);
+  }
 }
 
