35f5eac4a78a4d5c9c6958ac85cc41475ec2c14f
Steven Tieu <103811649+SteveT10@users.noreply.github.com>
2023-06-19 21:24:40 -0700

    Cubes now randomize colors

--- scripts/index.js
+++ scripts/index.js
@@ -87,15 +87,8 @@ import Background from "./Background.js";
   let cubeMesh = backgroundObj.cubeMesh;
   //const testShape = new THREE.Object3D();
 
-  /** COLOR SETTING INSTANCES ****/
-  //NOTE: You need to perform setColorAt() for all instances 
-  //or else instance color is null and unreadable, see Background.js.
-  let cubeColor = new THREE.Color();
-  cubeMesh.getColorAt(1, cubeColor);
-  cubeColor.setColorName('red');
-  cubeMesh.setColorAt(1, cubeColor );
-  cubeMesh.instanceColor.needsUpdate = true;
-  /************BELOW ARE CONSTS USED IN INSTANCE CUBE ROTATION, STILL WIP ******************************/
+
+  /************BELOW ARE CONSTS USED IN INSTANCE CUBE ROTATION, STILL WIP ***********************/
   /*Steps to rotate instances cubes
     1. Create a Quaternion, this represents a rotation 'vector'
     2. Create a Matrix4, we will multiple the existing matrix by this.
@@ -108,9 +101,17 @@ import Background from "./Background.js";
   const tmpQ = new THREE.Quaternion();
   const tmpM = new THREE.Matrix4();
   const currentM = new THREE.Matrix4();
-  //SRC: https://threejs.org/examples/?q=instanc#webgl_buffergeometry_instancing_interleaved
-  //TO RESEARCH
-    //https://threejs.org/docs/#api/en/math/Quaternion 
+
+  /** COLOR SETTING INSTANCES ****/
+  //NOTE: You need to perform setColorAt() for all instances 
+  //or else instance color is null and unreadable, see Background.js.
+  let cubeColor = new THREE.Color();
+  cubeMesh.getColorAt(1, cubeColor);
+  cubeColor.setColorName('red');
+  cubeMesh.setColorAt(1, cubeColor );
+  cubeMesh.instanceColor.needsUpdate = true;
+
+  //let previousIds = [];
   function animate(time) {
     //time *= 0.001; //convert to seconds.
     time = performance.now();
@@ -130,6 +131,23 @@ import Background from "./Background.js";
 				cubeMesh.setMatrixAt( i, currentM);
 
 			}
+
+      /******* Randomize Colors WIP ********/
+      for(let i = 0; i < 100; i++) {
+        //TODO revert the colors back after a set time interval.
+
+        //Change colors of new ones. Note that this doesn't account for duplicates.
+        let currentId = Math.ceil(Math.random() * cubeMesh.count);
+        //previousIds.push(currentId);
+        cubeMesh.getColorAt(currentId, cubeColor); //Store at cubeColor
+        cubeColor.setHex(Math.random() * 0xFFFFFF); 
+        //cubeColor.setHex(Math.random() * 0x30D5C8); //change color to miku.
+        cubeMesh.setColorAt(currentId, cubeColor); //Set color again.
+      }
+      cubeMesh.instanceColor.needsUpdate = true;
+
+
+
 
       cubeMesh.instanceMatrix.needsUpdate = true;
 			cubeMesh.computeBoundingSphere();
