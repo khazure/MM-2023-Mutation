e3828d9dd5935be20ffd037b89bebea8d75ab7a2
khazure <bunnystormdown@gmail.com>
2023-06-23 18:49:03 -0700

    pop up windows styling and function

--- scripts/Lyrics.js
+++ scripts/Lyrics.js
@@ -28,11 +28,16 @@ import { Player } from "textalive-app-api";
     // keeps track of current beat
     let currBeat = null;
 
+    // keeps track of current chord
+    let currChord = null;
+
     // keeps track if in chorus or not
     let inChorus = false;
 
     // keeps track of starting time for miku's speech in parenthesises
     let parenStart = null;
+
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 
   /**
    * Called when all TextAlive processes are done loading
@@ -55,9 +60,52 @@ import { Player } from "textalive-app-api";
     id("chorus-btn").addEventListener("click", jumpChorus);
     id("jump-btn").addEventListener("click", jumpMusic);
 
-    qs('#loading button').classList.remove("hidden");
-    qs('#loading p').classList.add("hidden");
+    qs('.window-body').classList.remove("hidden");
+    qs('.window > p').classList.add("hidden");
 
+    // enable drag on draggable elements
+    qsa(".window").forEach(element => {
+      element.querySelector(".window-header").addEventListener("mousedown", dragMouseDown);
+    })
+  }
+
+  function enableDrag(element) {
+    console.log(element.querySelector(".window-header"));
+    element.querySelector(".window-header").addEventListener("mousedown", dragMouseDown);
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    let currTarget = e.currentTarget.parentNode;
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = (event) => {
+      elementDrag(event, currTarget);
+    };
+  }
+
+  function elementDrag(e, elmnt) {
+    e = e || window.event;
+    e.preventDefault();
+    console.log(elmnt)
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
   }
 
   /**
@@ -173,94 +221,6 @@ import { Player } from "textalive-app-api";
     }
   }
 
-  function textHighlighting() {
-     // unit is at word level
-     if (unit.contains(now)) {
-      if (currPhrase === null || currPhrase !== unit.parent) {
-
-        // if new phrase, create the span objects
-        currPhrase = unit.parent;
-        let textContainer = id("text1");
-
-        // clear previous
-        textContainer.innerHTML = "";
-
-        unit.parent.children.forEach(child => {
-          let word = document.createElement("span");
-          word.textContent = child.text;
-          word.id = child.text.replace(" ", "-");
-
-          textContainer.append(word);
-        });
-      }
-
-      // if word is new,
-      // then identify current word in span objects and highlight
-      if (currWord === null || currWord !== unit) {
-        currWord = unit;
-
-        let activeWord = qs(".active-word")
-        if (activeWord) {
-          activeWord.classList.remove("active-word");
-        }
-        let wordId = unit.text.replace(" ", "-");
-   
-
-        id(wordId).classList.add("active-word"); 
-      }
-    }
-  }
-
-  function oldTextMorphing() {
-    if (unit.contains(now)) {
-      id("text1").textContent = unit.text;
-      id("text2").textContent = unit.next.text;  
-
-      let unitInterval = unit.endTime - unit.startTime;
-
-      // num from 0 to 1 representing progress of current unit. x
-      let intervalFrac = (now - unit.startTime) / unitInterval;
-
-      // follows an upsidedown parabola, max is 1, min is 0
-      let y = -Math.pow(((2 * intervalFrac) - 1), 2) + 1;
-
-      //id("text1").style.opacity = y;
-      id("text1").style.filter = "blur(" + (1 - y) * 6 + "px)";
-
-      // if next phrase comes within morphTime
-      // if (unit.next.startTime - now <= morphTime) {
-      //   const diff = (unit.next.startTime - now) / morphTime;
-      //   console.log("morph into next word with diff: " + diff);
-
-      //   id("text1").textContent = unit.text;
-      //   id("text2").textContent = unit.next.text;  
-
-      //   // here, diff 1 --> 0
-      //   id("text1").style.opacity = diff;
-      //   id("text2").style.opacity = 1 - diff;
-      // } else if (now - unit.previous.endTime < morphTime) {
-      //   // if current phrase is within [morphTime] of the previous page
-      //   const diff = (now - unit.previous.endTime) / morphTime;
-
-      //   console.log("morphing from last word with diff: " + diff)
-
-      //   id("text1").textContent = unit.previous.text;
-      //   id("text2").textContent = unit.text;  
-
-      //   // here, diff 0 --> 1
-      //   id("text1").style.opacity = 1 - diff;
-      //   id("text2").style.opacity = diff;
-      // } else {
-      //   id("text1").textContent = unit.text;
-      //   id("text2").textContent = unit.next.text;  
-
-      //   console.log("solid text");
-      //   id("text1").style.opacity = 1;
-      //   id("text2").style.opacity = 0;
-      // }
-    }
-  }
-
   player.addListener({
     onAppReady,
     onVideoReady,
@@ -348,6 +308,13 @@ import { Player } from "textalive-app-api";
       id("beat-reactor").style.scale = "1";
     }
 
+    // animate if new chord encountered
+    let chord = player.findChord(position);
+    if (chord && chord != currChord) {
+      currChord = chord;
+      qs("nav").style.backgroundColor = random_rgba();
+    }
+
     if (player.findChorus(position)) {
       qs(".grid").classList.replace("non-chorus", "chorus");
     } else {
@@ -378,16 +345,6 @@ import { Player } from "textalive-app-api";
     init();
   }
 
-  // https://codepen.io/alvarotrigo/pen/eYEqPZa 
-  // new plan:
-  // - doMorph when phrasechange detected within a time frame
-  // --> ex. 0.25s to nextphrase's start time, initiate morph (morph out)
-  // --> ex. 0.25s after currentPhrase's start time, be morphing (morph in)
-  // - else doCooldown 
-  //    
-  function morphAnimate(time) {
-  }
-
   function id(name) {
     return document.getElementById(name);
   }
@@ -410,5 +367,13 @@ import { Player } from "textalive-app-api";
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
 }
+
+// https://stackoverflow.com/questions/23095637/how-do-you-get-random-rgb-in-javascript
+function random_rgba() {
+  var o = Math.round, r = Math.random, s = 255;
+  return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ', 1)';
+  //return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+}
+
 }());
 
